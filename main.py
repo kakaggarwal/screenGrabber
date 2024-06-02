c9758a366d5b17b58920bc3abc9ff8a1ec1fa5d7
Krishan Aggarwal <kakaggarwal@gmail.com>
2024-06-02 16:11:43 +0530

    feat: Screen grab function created

---
+++ main.py
@@ -0,0 +1,19 @@
+from PIL import ImageGrab
+import datetime
+import time
+import os
+
+def grabScreenFunc():
+    screenGrab = ImageGrab.grab(None, True, True)
+
+    fileName = datetime.datetime.now().strftime(".\captures\%Y-%m-%dT%H-%M-%S.png")
+
+    screenGrab.save(fileName, 'PNG')
+
+if not os.path.exists("./captures"):
+    os.makedirs("./captures")
+
+while(True):
+    grabScreenFunc()
+    # Capturing Screen every 30 seconds
+    time.sleep(30)
